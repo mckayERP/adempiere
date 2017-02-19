21871fc710b83b20c78f87c35e7a1ef3733130dc
mckayERP <michael.mckay@mckayERP.com>
2017-02-19 09:27:52 -0500

    FIFO/LIFO: Update VAttributeGrid to use new call to find storage locations.

--- client/src/org/compiere/apps/form/VAttributeGrid.java
+++ client/src/org/compiere/apps/form/VAttributeGrid.java
@@ -518,7 +518,7 @@ public class VAttributeGrid extends CPanel
 		formatted = "";
 		if (m_M_Warehouse_ID != 0)
 		{
-			BigDecimal qty = MStorage.getQtyAvailable(m_M_Warehouse_ID, M_Product_ID, 0, null);
+			BigDecimal qty = MStorage.getQtyAvailable(m_M_Warehouse_ID, 0, M_Product_ID, 0, null);
 			if (qty == null)
 				formatted = "-";
 			else
